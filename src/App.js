09bdf8a2a7a8eb81c9a1c09b988b9be710821074
AbdulazizProgrammer <Abdulazizprogrammer@gmail.com>
2021-01-13 17:07:12 +0500

    sevenReactSite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar'
+import TourList from './components/TourList'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <TourList />
+    </React.Fragment>
   );
 }
 
